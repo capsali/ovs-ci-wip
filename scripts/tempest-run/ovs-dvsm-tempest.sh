014469365240a908b09e258183daeaf13f1adb64
mcapsali <mgheorghe@cloudbasesolutions.com>
2017-02-21 16:50:46 +0200

    edited deployer args

--- scripts/tempest-run/ovs-dvsm-tempest.sh
+++ scripts/tempest-run/ovs-dvsm-tempest.sh
@@ -34,7 +34,7 @@ EOF
 " >> $BUNDLE_LOCATION
 
 
-$DEPLOYER_PATH/deployer.py  --clouds-and-credentials $DEPLOYER_PATH/ci-cl-creds.yaml deploy --template $BUNDLE_LOCATION --search-string $UUID
+$DEPLOYER_PATH/deployer.py  --clouds-and-credentials $DEPLOYER_PATH/ci-cl-creds.yaml deploy --template $BUNDLE_LOCATION --max-unit-retries 5 --timeout 7200 --search-string $UUID
 build_exit_code=$?
 
 source $WORKSPACE/nodes
